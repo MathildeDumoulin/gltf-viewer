db868f7c5230aa668787950750b2b9e3edfdce10
Mathilde <mathildedumoulin54@gmail.com>
2022-04-05 14:06:17 +0200

    [shaders] Add fragment shader

--- src/ViewerApplication.hpp
+++ src/ViewerApplication.hpp
@@ -48,7 +48,7 @@ private:
 
   fs::path m_gltfFilePath;
   std::string m_vertexShader = "forward.vs.glsl";
-  std::string m_fragmentShader = "normals.fs.glsl";
+  std::string m_fragmentShader = "diffuse_directionnal_light.fs.glsl";
 
   bool m_hasUserCamera = false;
   Camera m_userCamera;
